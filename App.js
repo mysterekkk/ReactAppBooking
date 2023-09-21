2a3f92ab7e380cab16baf78aa5427a5fa2831347
Łukasz Rosikoń <91424068+mysterekkk@users.noreply.github.com>
2023-09-22 01:48:03 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import BookingForm from './components/BookingForm';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Little Lemon Booking</h1>
+            <BookingForm />
+        </div>
+    );
+}
+
+export default App;
